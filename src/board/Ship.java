7f6e19598581ee47fa52a973b9da4a031fdba528
Luca de Roldao <lucarolda11@gmail.com>
2023-03-06 19:08:55 -0300

    fix(): ships documentation

--- src/board/Ship.java
+++ src/board/Ship.java
@@ -13,15 +13,14 @@ public class Ship {
 	}
 
 	public static int[][] getShipTypes() {
-		// { 1 -> quantidade de peças, 5 -> quantidade de espaços  que a peca ocupa }
-		// { 1, 5 }, - 1 porta avioes
-		//	{ 2, 4 } - 2 navios de 4 canos
-		//	{ 3, 2 } - 3 navios de 2 canos
-		// { 4, 1 } - 4 navios de 1 cano
-		int[][] shipTypes = { { 1, 5 }};
 		// { 1 -> quantidade de peças no tabuleiro, 5 -> quantidade de espaços no tabuleiro }
-		// { 1, 5 }, { 2, 4 }, { 3, 3 }, { 4, 2 }
-		//int[][] shipTypes = { { 1, 5 }, { 2, 4 }, { 3, 3 }, { 4, 2 } };
+		// { 1, 5 }, - 1 navio de 5 canos
+		// { 2, 4 } - 1 navio de 4 canos
+		// { 2, 3 } - 2 navios de 3 canos
+		// { 3, 2 } - 3 navios de 2 canos
+		// { 4, 1 } - 4 navios de 1 cano
+		int[][] shipTypes = { { 1, 5 }, { 1, 4 }, { 3, 2 }, { 4, 1 } };
+
 		return shipTypes;
 	}
 }
